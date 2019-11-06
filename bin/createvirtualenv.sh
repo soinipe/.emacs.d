0e98e7f8ede36ba7a3507775bd13e0363ffd273d
soinipe <petri.soininen@gmail.com>
2019-11-06 16:58:22 +0200

    Update createvirtualenv.sh

--- bin/createvirtualenv.sh
+++ bin/createvirtualenv.sh
@@ -13,6 +13,8 @@ echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.bashrc
 echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
 source ~/.bashrc
 mkvirtualenv p2 -p python2
+# For Ubuntu 18.04, workaround for ModuleNotFoundError: No module named 'distutils.spawn'
+# sudo apt-get install python3-distutils
 mkvirtualenv p3 -p python3
 
 # End of file
